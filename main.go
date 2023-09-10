fa54a346d036e17d7650d7474317e5b50f1f2167
knalibay <kanatbekati@gmail.com>
2023-09-10 18:54:03 +0600

    ascii

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// Обработчик главной страницы.
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Привет из Snippetbox"))
+}
+
+// Обработчик для отображения содержимого заметки.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Отображение заметки..."))
+}
+
+// Обработчик для создания новой заметки.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Форма для создания новой заметки..."))
+}
+
+func main() {
+	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
+	// маршрутизаторе servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Запуск веб-сервера на http://127.0.0.1:5500")
+	err := http.ListenAndServe(":5500", mux)
+	log.Fatal(err)
+}
